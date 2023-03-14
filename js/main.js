27c609fbc7cf5b3f8f9573a66dc4d235ac6f258d
Alex Nunzi <alex.nunzi@hotmail.it>
2023-03-14 14:55:00 +0100

    Bonus 2: Al' apertura della pagina viene visualizzato il logo di JS e al click su di esso compare il logo di Vue cambiando anche il contenuto di h1 e la classe che gestisce il colore del testo

--- js/main.js
+++ js/main.js
@@ -3,9 +3,19 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            welcome: 'Hello Vue JS!',
-            img: 'img/logo-vue.png',
-            imgDescription: 'Logo of Vue'
+            welcome: 'Hi, JS!',
+            img: 'img/logo-js.png',
+            classi: 'text-js',
+            imgDescription: 'Logo of Vue',
+            userInfo: 'Premi sul logo per potenziarlo!'
         }
     },
+    methods: {
+        upgrade(){
+            this.img = 'img/logo-vue.png';
+            this.classi = 'text-vue';
+            this.welcome = 'Welcome Vue JS!';
+            this.userInfo = '';
+        }
+    }
 }).mount('#app')
